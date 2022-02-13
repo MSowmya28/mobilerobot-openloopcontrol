f51238c668ff2ab608993f8fbcf9ba5418b32787
MSowmya28 <sowmya281203@gmail.com>
2022-02-13 20:44:15 +0530

    https://github.com/MSowmya28/mobilerobot-openloopcontrol.git

--- linear_movement.py
+++ linear_movement.py
@@ -15,8 +15,11 @@ if __name__ == '__main__':
     '''
     ep_chassis.move(x=2, y=0, z=0, xy_speed=0.75).wait_for_completed()
 
-    ep_chassis.move(x=0, y=0, z=90, xy_speed=1).wait_for_completed()
+    ep_chassis.move(x=0, y=0, z=50, xy_speed=1).wait_for_completed()
 
-    ep_chassis.move(x=2, y=0, z=0, xy_speed=0.75).wait_for_completed()
+    ep_chassis.move(x=3.5, y=0, z=0, xy_speed=0.75).wait_for_completed()
+
+   
+
 
     ep_robot.close()
